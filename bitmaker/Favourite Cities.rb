9ed8a88f119f3f44be91596301d3832035cd8243
Paul Walsh <twentytwentyk@live.com>
2017-01-09 12:44:03 -0500

    Commit message

---
+++ bitmaker/Favourite Cities.rb
@@ -0,0 +1,4 @@
+  cities = ['Toronto', 'Hong Kong', 'Seoul', 'Denver', 'Ottawa', 'BitMaker']
+  cities.each{|city|
+    puts "#{city}"
+  }
---
+++ bitmaker/calc.rb
@@ -0,0 +1,6 @@
+puts "Enter a number"
+x = gets.chomp().to_i
+puts "Enter another number"
+y = gets.chomp().to_i
+result = x + y
+puts "If you add them together you get #{result}"
